30a0797cb0289b6841578171ca903d8ef69670c0
Rich Kevan <rskevan@me.com>
2024-06-18 15:23:19 -0700

    removed image from nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -7,12 +7,6 @@ const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
   images: {
     formats: ["image/avif", "image/webp"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
   }
 }
 
