08c200295bbdf7e258434dc59ae4568700283afe
Rich Kevan <rskevan@me.com>
2024-06-26 13:00:18 -0700

    Updated route Mods to main.yaml workflow

---
+++ app/pathways/page.tsx
@@ -0,0 +1,12 @@
+import { SimpleLayout } from "@/components/SimpleLayout"
+
+export default function Learning() {
+  return (
+    <SimpleLayout
+    title="Pathways, mapping the progress of my learning and development journey"
+    intro="As a dedicated software engineer, I am committed to continuous learning and growth. Here, you'll find a record of my courses and professional development efforts. This space reflects my passion for exploring new technologies and advancing my skills. Join me on this journey of discovery and innovation."
+    >
+      <h1>Learning</h1>
+    </SimpleLayout>
+  )
+}
