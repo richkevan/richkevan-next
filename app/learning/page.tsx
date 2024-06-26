e8e1ea79646e1970dbde7b1da4157216cdbe415c
Rich Kevan <rskevan@me.com>
2024-06-26 12:37:30 -0700

    Added learning page

---
+++ app/learning/page.tsx
@@ -0,0 +1,12 @@
+import { SimpleLayout } from "@/components/SimpleLayout"
+
+export default function Learning() {
+  return (
+    <SimpleLayout
+    title="Learning"
+    intro="Learning"
+    >
+      <h1>Learning</h1>
+    </SimpleLayout>
+  )
+}
